a6e064d0abe11020d8fe4c1e74453ad07d692126
Benedikt Lüth <benedikt.lueth@gmail.com>
2021-04-26 09:58:49 +0200

    Vorbereitung des vierten Tutoriums

---
+++ bwi231_211/src/de/hs_lu/o2s/tut4/ueb/Boot.java
@@ -0,0 +1,65 @@
+package de.hs_lu.o2s.tut4.ueb;
+
+public class Boot {
+
+	private int gewicht, sitze, maxKnoten;
+	private double laenge, breite, hoehe;
+
+	public Boot(int gewicht, int sitze, int maxKnoten, double laenge, double breite, double hoehe) {
+		this.gewicht = gewicht;
+		this.sitze = sitze;
+		this.maxKnoten = maxKnoten;
+		this.laenge = laenge;
+		this.breite = breite;
+		this.hoehe = hoehe;
+	}
+
+	public int getGewicht() {
+		return this.gewicht;
+	}
+
+	public void setGewicht(int gewicht) {
+		this.gewicht = gewicht;
+	}
+
+	public int getSitze() {
+		return this.sitze;
+	}
+
+	public void setSitze(int sitze) {
+		this.sitze = sitze;
+	}
+
+	public int getMaxKnoten() {
+		return this.maxKnoten;
+	}
+
+	public void setMaxKnoten(int maxKnoten) {
+		this.maxKnoten = maxKnoten;
+	}
+
+	public double getLaenge() {
+		return this.laenge;
+	}
+
+	public void setLaenge(double laenge) {
+		this.laenge = laenge;
+	}
+
+	public double getBreite() {
+		return this.breite;
+	}
+
+	public void setBreite(double breite) {
+		this.breite = breite;
+	}
+
+	public double getHoehe() {
+		return this.hoehe;
+	}
+
+	public void setHoehe(double hoehe) {
+		this.hoehe = hoehe;
+	}
+
+}
